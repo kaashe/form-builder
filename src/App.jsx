9391440b2255105b33adcf336056ad3ee89333e4
Kashif Ullah <43569363+kaashe@users.noreply.github.com>
2024-12-30 11:07:21 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormCreator from "./components/FormCreator";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <FormCreator />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"], // This enables Tailwind for your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
